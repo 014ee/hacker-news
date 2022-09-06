7aa02a6de8f734650549497ebb0d0eb7658d9db0
inhwalee <014ee@naver.com>
2022-09-06 15:54:07 +0900

    Fix: 사파리에서 지원 안하는 문법 수정

--- src/utils/regExp.js
+++ src/utils/regExp.js
@@ -1,4 +1,5 @@
 export const getOnlyDomainName =
   /(http(s)?:\/\/)([a-z0-9\w]+\.*)+[a-z0-9]{2,4}/gi;
 
-export const getOnlyPageName = /(?<=\/).*?(?=\/)/;
+// export const getOnlyPageName = /(?<=\/).*?(?=\/)/; 사파리에서 lookbehind 지원 안함
+export const getOnlyPageName = /[^/].*?(?=\/)/;
