cfa29f9a0236ac5a181d095458164ef9b44f7f25
inhwalee <014ee@naver.com>
2022-09-08 11:09:53 +0900

    Fix: 안쓰는 툴 주석처리

--- src/utils/regExp.js
+++ src/utils/regExp.js
@@ -1,5 +1,3 @@
 export const getOnlyDomainName =
   /(http(s)?:\/\/)([a-z0-9\w]+\.*)+[a-z0-9]{2,4}/gi;
-
-// export const getOnlyPageName = /(?<=\/).*?(?=\/)/; 사파리에서 lookbehind 지원 안함
 export const getOnlyPageName = /[^/].*?(?=\/)/;
